aa90861b886237a4bc55ca21637863d7fa8ecd41
muhammedshameem33 <shameempp33@gmail.com>
2020-09-04 23:25:53 +0530

    [muhammed shameem] add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxSpinnerModule } from "ngx-spinner";
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './service/http.service';
 import { PostsComponent } from './components/posts/posts.component';
 
@@ -19,7 +21,9 @@ import { PostsComponent } from './components/posts/posts.component';
     BrowserModule,
     AppRoutingModule,
     NgxSpinnerModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from 'src/app/service/http.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpService } from 'src/app/service/http.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-posts',
@@ -10,7 +11,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class PostsComponent implements OnInit {
 
   constructor(private http:HttpService,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getAllPosts();
@@ -24,10 +26,18 @@ export class PostsComponent implements OnInit {
       res => {
         console.log(res);
         this.spinner.hide();
+        this.openSnackBar("Successfully get all posts","Dismiss");
       },
       error => {
         this.spinner.hide();
+        this.openSnackBar("Failed During fetching","Dismiss");
       }
     );
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
