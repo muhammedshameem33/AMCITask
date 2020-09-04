f80880484283e44724c8e66f6b99d11e1c75daeb
muhammedshameem33 <shameempp33@gmail.com>
2020-09-04 22:41:19 +0530

    [muhammed shameem] add http methods

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public get(url:string){
+    return this.http.get(environment.baseUrl+url);
+  }
+
+  public put(url:string,data){
+    return this.http.put(environment.baseUrl+url,data);
+  }
+
+  
 }
