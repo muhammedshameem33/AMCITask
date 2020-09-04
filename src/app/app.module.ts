e2cb85b0f8145b697b9a180bfd77ef7d529e07a2
muhammedshameem33 <shameempp33@gmail.com>
2020-09-04 22:45:22 +0530

    [muhammed shameem] add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MaterialModule } from './material/material.module';
 import { HttpService } from './service/http.service';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
