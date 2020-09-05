b9421c5f50af0c9a7f26b337ca49cc79e8defd6e
muhammedshameem33 <shameempp33@gmail.com>
2020-09-05 15:29:55 +0530

    [muhammed shameem] add dialogue box

--- src/app/components/edit-post-component/edit-post-component.component.ts
+++ src/app/components/edit-post-component/edit-post-component.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional, Inject, Input } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpService } from 'src/app/service/http.service';
+import { Post } from 'src/app/Models/post';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-edit-post-component',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-post-component.component.css']
 })
 export class EditPostComponentComponent implements OnInit {
+  fromPage: Post;
+  @Input() title:any;
+  @Input() body:any;
 
-  constructor() { }
-
+  constructor(private http:HttpService, 
+    private snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<EditPostComponentComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) { 
+      this.fromPage = data.pageValue;
+      console.log(this.fromPage)
+    }
+    apiCallUpdate(){
+      let data={
+        title:this.title,
+        body:this.body
+      }
+      this.http.put('posts/'+this.fromPage.id,data).subscribe(
+        res=>{
+          this.openSnackBar("Successfully updated","Dismiss");
+          console.log(res);
+        },
+        error=>{
+          this.openSnackBar("Update Failed","Dismiss");
+          console.log(error);
+        }
+      )
+    }
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+    });
+  }
   ngOnInit(): void {
   }
+
+
 
 }
