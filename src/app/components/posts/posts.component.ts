f52080a3dd5dccc2b3dc7557cf94e03f7d0f1930
muhammedshameem33 <shameempp33@gmail.com>
2020-09-05 06:15:40 +0530

    [muhammed shameem] add paginator

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpService } from 'src/app/service/http.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Post } from 'src/app/Models/post';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit,AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 dataSource:any;
-displayedColumns:string[]=['id','userId','title','body']
+displayedColumns:string[]=['id','userId','title','body','actions','delete']
   constructor(private http:HttpService,
     private spinner: NgxSpinnerService,
     private snackBar: MatSnackBar) { }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
     this.getAllPosts();
+    
   }
 
-
   private getAllPosts() {
-    debugger
     this.spinner.show();
     this.http.get('posts').subscribe(
       res => {
